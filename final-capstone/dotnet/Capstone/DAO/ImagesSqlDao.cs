1ca9071ab692ee75a2beaab992501f16162bbbac
Matthew Lawson <matthewlawson000@gmail.com>
2021-12-07 13:56:46 -0500

    first DAO

---
+++ final-capstone/dotnet/Capstone/DAO/ImagesSqlDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Models;
+
+namespace Capstone.DAO
+{
+    public class ImagesSqlDao
+    {
+        
+       // public string GetImg();
+       //{
+
+       //}
+    }
+}
---
+++ final-capstone/dotnet/Capstone/DAO/PropertiesSqlDao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Models;
+using System.Data.SqlClient;
+
+
+namespace Capstone.DAO
+{
+    public class PropertiesSqlDao : IPropertiesDao
+    {
+        private readonly string connectionString;
+        public PropertiesSqlDao(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+        public IList<Properties> GetAllProperties()
+        {
+            IList<Properties> properties = new List<Properties>();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var sql = 
+                    "";
+
+            }
+         
+        }
+    }
+}
