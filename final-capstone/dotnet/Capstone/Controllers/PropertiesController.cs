2395e65d32c00297ddffbe37bc1f87984110b4d3
Brad <bradley.l.quinton@gmail.com>
2021-12-07 15:59:05 -0500

    db changes

---
+++ final-capstone/dotnet/Capstone/Controllers/PropertiesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Capstone.DAO;
+using Capstone.Models;
+
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+
+    public class PropertiesController : ControllerBase
+    {
+        private readonly IPropertiesDao _propertiesDao;
+
+        public PropertiesController(IPropertiesDao propertiesDao)
+        {
+            _propertiesDao = propertiesDao;
+        }
+
+
+        [HttpGet]
+        public ActionResult<List<Properties>> GetAllProperties()
+        {
+            return Ok(_propertiesDao.GetAllProperties());
+        }
+
+        
+
+        
+    }
+}
