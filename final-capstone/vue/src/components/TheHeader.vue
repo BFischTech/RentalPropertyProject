aeb12688154bebe4680ac91ab271b99189e757d4
Brad <bradley.l.quinton@gmail.com>
2021-12-08 19:20:29 -0500

    latest front end changes

---
+++ final-capstone/vue/src/components/TheHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="the-header">test</div>
+</template>
+
+<script>
+export default {
+  name: "the-header",
+};
+</script>
+
+
+<style scoped>
+.the-header {
+  grid-area: header;
+  background-color: #f69a00;
+}
+
+.container {
+  display: grid;
+  grid-template-areas: "the-header the-header the-header";
+  grid-template-columns: 200px 1fr 200px;
+  gap: 10px;
+  
+}
+
+@media screen and (max-width: 1024px) {
+  .container {
+    grid-template-areas: "header header";
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .container {
+    grid-template-areas: "header";
+    grid-template-columns: 1fr;
+  }
+}
+</style>
--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home">
+    <the-header></the-header>
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
 </template>
 
 <script>
+
+import TheHeader from "../components/TheHeader.vue";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    TheHeader
+  }
 };
 </script>
