b79f973e01efe604d3107f8b7fdb56f2d5ba880c
Brad <bradley.l.quinton@gmail.com>
2021-12-10 00:24:52 -0500

    rent payment form

--- final-capstone/vue/src/components/RentPaymentForm.vue
+++ final-capstone/vue/src/components/RentPaymentForm.vue
@@ -1,44 +1,121 @@
 <template>
-<div>
-    <b-container fluid>
-  <b-row class="my-1">
-    <b-col sm="2">
-      <label for="input-small">Small:</label>
-    </b-col>
-    <b-col sm="10">
-      <b-form-input id="input-small" size="sm" placeholder="Enter your name"></b-form-input>
-    </b-col>
-  </b-row>
+  <div>
+    <b-card bg-variant="light">
+      <p id="submitFormHeading">Submit Rent Form</p>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group id="rentPaymentFormGroup">
+          <p id="inputPaymentDate">Choose a payment date</p>
+          <b-form-datepicker
+            label
+            for="datepicker"
+            id="inputDatepicker"
+            v-model="date"
+            class="datepicker"
+          ></b-form-datepicker>
+          <!-- <p>Date Chosen: {{ value }}</p> -->
 
-  <b-row class="my-1">
-    <b-col sm="2">
-      <label for="input-default">Default:</label>
-    </b-col>
-    <b-col sm="10">
-      <b-form-input id="input-default" placeholder="Enter your name"></b-form-input>
-    </b-col>
-  </b-row>
+          <p id="propertyToChoose">Choose a property</p>
+          <b-form-select
+            id="inputPropertySelect"
+            v-model="form.property"
+            :options="properties"
+            required
+          ></b-form-select>
 
-  <b-row class="my-1">
-    <b-col sm="2">
-      <label for="input-large">Large:</label>
-    </b-col>
-    <b-col sm="10">
-      <b-form-input id="input-large" size="lg" placeholder="Enter your name"></b-form-input>
-    </b-col>
-  </b-row>
-</b-container>
-</div>
-  
+          <p id="rentAmount">Enter payment amount</p>
+          <b-form-input
+            id="inputRent"
+            v-model="form.rent"
+            type="number"
+            pattern="^\$\d{1,3}(,\d{3})*(\.\d+)?$"
+            value=""
+            data-type="currency"
+            placeholder="$1.00"
+            required
+          >
+          </b-form-input>
+
+          <b-form-checkbox-group v-model="form.checked" id="inputCheckbox">
+            <b-form-checkbox value="agree"
+              >&nbsp; I agree to the terms and conditions</b-form-checkbox
+            >
+          </b-form-checkbox-group>
+        </b-form-group>
+        
+        <b-form-group class="buttons">
+          <b-button class="submitButton" type="submit" variant="primary"
+            >Submit</b-button
+          >
+          <b-button class="resetButton" type="reset" variant="danger"
+            >Reset</b-button
+          >
+        </b-form-group>
+      </b-form>
+    </b-card>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      form: {
+        date: "",
+        property: null,
+        checked: [],
+      },
+      properties: [
+        { text: "Select One", value: null },
+        "Example Property 1",
+        "Example Property 2",
+        "Example Property 3",
+        "Example Property 4",
+      ],
+      show: true,
+      date: "",
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.date = "";
+      this.form.property = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+#submitFormHeading {
+  text-align: center;
+  font-size: 2rem;
+  text-transform: uppercase;
+}
 
+#propertyToChoose {
+  padding: 50px 0px 0px 0px;
+}
 
+#rentAmount {
+  padding: 50px 0px 0px 0px;
+}
+
+#inputCheckbox {
+  padding: 50px 0px 0px 0px;
+}
+
+.buttons {
+  padding: 50px 0px 0px 0px;
+}
 </style>
