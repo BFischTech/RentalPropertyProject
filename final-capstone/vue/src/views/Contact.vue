61d9aff295ad673876f626057f2b546079c9381f
Bradley Fisch <bfischtech@gmail.com>
2021-12-09 19:52:41 -0500

    front end cleanup / design

---
+++ final-capstone/vue/src/views/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+ <contact-form></contact-form>
+</template>
+
+
+<script>
+
+import ContactForm from '../components/ContactForm.vue'
+
+export default {
+  components: { ContactForm },
+name: 'Contact',
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ final-capstone/vue/src/views/Renter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <rent-payment-form></rent-payment-form>
+  </div>
+</template>
+
+<script>
+
+    import RentPaymentForm from '../components/RentPaymentForm.vue'
+    
+    export default {
+        name: 'Renter',
+        components: 
+        {
+            RentPaymentForm
+        },
+    
+    }
+
+</script>
+
+<style>
+
+</style>
