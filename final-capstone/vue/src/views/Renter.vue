b2322bbe781fde1c6009f9e92496810c7f820343
bfischtech(PC) <bfischtech@gmail.com>
2021-12-13 21:24:12 -0500

    front end coming together, renter page updated and home

--- final-capstone/vue/src/views/Renter.vue
+++ final-capstone/vue/src/views/Renter.vue
@@ -1,7 +1,10 @@
 <template>
+  <div id="container-grid"><h1 id="header">Welcome to the tenant portal, how may we help you?</h1>
+  <h2 id="greeting">Please don't hesitate to <a href="/contact">contact us</a>!</h2>
   <div>
-      <rent-payment-form></rent-payment-form>
-  </div>
+    <b-button id="form" v-b-modal.form-modal>Rental Payment</b-button>
+      <b-modal id="form-modal"><rent-payment-form></rent-payment-form></b-modal>
+  </div></div>
 </template>
 
 <script>
@@ -19,6 +22,23 @@
 
 </script>
 
-<style>
+<style scoped>
+
+#container-grid {
+  display: grid;
+  grid-template-columns: 30% 40% 30%;
+ 
+  row-gap: 5px;
+  column-gap: 5px;
+ 
+   padding-left: 15px;
+  justify-content: space-evenly;
+  justify-items: center;
+  align-content: space-evenly;
+  align-items: center;
+}
+
+
+
 
 </style>
