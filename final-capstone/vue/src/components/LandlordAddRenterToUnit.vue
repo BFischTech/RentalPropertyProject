0d2e0c964320722877e2260b8da875f4f192a385
Clark Mercado <clarkjtm@gmail.com>
2021-12-15 23:36:42 -0500

    ...

---
+++ final-capstone/vue/src/components/LandlordAddRenterToUnit.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div id="divOption">
+      <label for="">Select Unit Id:</label>&nbsp;
+        <b-form-select v-model="unitAndTenant.unitId" required>
+        <option value="" disabled>Unit Id</option>
+        <option v-for="unit in unitAndTenants.unit" :value="unit.unitId" :key="unit.unitId">{{ unit.unitId }}</option>
+       </b-form-select>
+    </div>
+
+    <div id="divOption">
+        <label for="">Select Tenant Id: </label>&nbsp;
+        <b-form-select v-model="unitAndTenant.tenantId" required>
+        <option value="" disabled>Tenant Id</option>
+        <option v-for="tenant in unitAndTenants.tenant" :value="tenant.tenantId" :key="tenant.tenantId">{{ tenant.tenantId }}</option>
+       </b-form-select>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import LandlordService from "../services/LandlordService";
+
+export default {
+  data() {
+    return {
+      unitAndTenants: [],  
+      unitAndTenant: {
+        unitId: "",
+        tenantId: ""    
+      },
+    };
+  },
+  created() {
+    LandlordService.GetAllAvailableUnitsByOwnerAndTenantWithoutRentedUnit().then(response => {
+      this.unitAndTenants = response.data;
+    });
+  },
+};
+</script>
+
+<style scoped>
+
+#divOption {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.26);
+  padding:  1rem;
+  text-align: center;
+}
+
+option{
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: green;
+  border: 1px solid green;
+  color: white;
+  cursor: pointer;
+}
+</style>
