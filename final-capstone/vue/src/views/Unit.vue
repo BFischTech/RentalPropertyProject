95fb512f4f81be84d91dd459acf86a45174ddbd2
Clark Mercado <clarkjtm@gmail.com>
2021-12-16 03:02:19 -0500

    ......

--- final-capstone/vue/src/views/Unit.vue
+++ final-capstone/vue/src/views/Unit.vue
@@ -12,30 +12,35 @@
           :img-src="unitImage.imageUrl"
         ></b-carousel-slide>
       </b-carousel>
-    <div v-for="unit in units.unit" :key="unit.unitId">
-      <b-card :title="unit.name" sub-title="(Available)" class="text-center"> 
-        <b-card-text>
-          <i>'Rent is &dollar;{{unit.rentAmount}} a month'</i><br><br>
-            <h5>&#8220;{{ unit.description }}&#8221;</h5>
-            <i class="fa fa-bed" aria-hidden="true"> {{ unit.bedroomCount }} Bedroom&#40;s&#41;</i> &nbsp;
-            <i class="fa fa-shower" aria-hidden="true">{{ unit.bathroomCount }} Bathroom&#40;s&#41;</i> <br>
-            <i class="fa fa-paw" aria-hidden="true" v-if="unit.petFriendly === true"> Pets allowed</i>
-            <i class="fa fa-paw" aria-hidden="true" v-else>No pets allowed</i><br>
-            <i v-if="unit.nonSmoking === true">Smoking allowed<br></i>
-            <i v-else>No smoking allowed<br></i>
-            <i v-if="unit.poolAccess === true">Has a pool<br></i> 
-            <i class="fa fa-car" aria-hidden="true">{{ unit.parkingSpots }} Parking spot&#40;s&#41;</i>
-        </b-card-text>
-      </b-card>
+      <div v-for="unit in units.unit" :key="unit.unitId">
+        <b-card :title="unit.name" sub-title="(Available)" class="text-center"> 
+          <b-card-text>
+            <i>'Rent is &dollar;{{unit.rentAmount}} a month'</i><br><br>
+              <h5>&#8220;{{ unit.description }}&#8221;</h5>
+              <i class="fa fa-bed" aria-hidden="true"> {{ unit.bedroomCount }} Bedroom&#40;s&#41;</i> &nbsp;
+              <i class="fa fa-shower" aria-hidden="true">{{ unit.bathroomCount }} Bathroom&#40;s&#41;</i> <br>
+              <i class="fa fa-paw" aria-hidden="true" v-if="unit.petFriendly === true"> Pets allowed</i>
+              <i class="fa fa-paw" aria-hidden="true" v-else>No pets allowed</i><br>
+              <i v-if="unit.nonSmoking === true">Smoking allowed<br></i>
+              <i v-else>No smoking allowed<br></i>
+              <i v-if="unit.poolAccess === true">Has a pool<br></i> 
+              <i class="fa fa-car" aria-hidden="true">{{ unit.parkingSpots }} Parking spot&#40;s&#41;</i>
+          </b-card-text>
+        </b-card>
+      </div>
+     <AddGoogleMap />
     </div>
-  </div>
+
   </div>
 </template>
 
 <script>
 import PropertyService from "../services/PropertyService.js";
-
+import AddGoogleMap from "../components/AddGoogleMap";
 export default {
+  components: { 
+    AddGoogleMap 
+  },
   name: "unitPage",
   data() {
     return {
@@ -52,6 +57,15 @@ export default {
 </script>
 
 <style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  margin-top: 50px;
+}
+
   #image {
     width: 30rem;
     height: 20rem;
